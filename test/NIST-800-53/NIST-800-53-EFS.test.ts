1333b17101f32ff7a96d4f74362865be9d2ca130
jen-rich <44377149+jen-rich@users.noreply.github.com>
2021-07-22 18:57:46 -0400

    Update test/NIST-800-53/NIST-800-53-EFS.test.ts
    Co-authored-by: Arun Donti <dontirun@gmail.com>

--- test/NIST-800-53/NIST-800-53-EFS.test.ts
+++ test/NIST-800-53/NIST-800-53-EFS.test.ts
@@ -1,38 +1,42 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
 import { SynthUtils } from '@aws-cdk/assert';
-import { Vpc} from '@aws-cdk/aws-ec2';
+import { Vpc } from '@aws-cdk/aws-ec2';
 import { FileSystem } from '@aws-cdk/aws-efs';
 import { Aspects, Stack } from '@aws-cdk/core';
 import { NIST80053Checks } from '../../src/NIST-800-53/nist-800-53';
 
 describe('Amazon Elastic File System (Amazon EFS)', () => {
-    test('efs-encrypted-check: Elastic File Systems are encrypted', () => {
-      const positive = new Stack();
-      Aspects.of(positive).add(new NIST80053Checks());
-      new FileSystem(positive, 'rEFS', {
-        vpc: new Vpc(positive, 'rVpc'),
-        encrypted: false,
-      });
-      const messages = SynthUtils.synthesize(positive).messages;
-      expect(messages).toContainEqual(
-        expect.objectContaining({
-          entry: expect.objectContaining({
-            data: expect.stringContaining('NIST.800.53-EFSEncryptedCheck:'),
-          }),
-        }),
-      );
-      const negative = new Stack();
-      Aspects.of(negative).add(new NIST80053Checks());
-      new FileSystem(negative, 'rEFS', {
-        vpc: new Vpc(negative, 'rVpc'),
-        encrypted: true,
-      });
-      const messages2 = SynthUtils.synthesize(negative).messages;
-      expect(messages2).not.toContainEqual(
-        expect.objectContaining({
-          entry: expect.objectContaining({
-            data: expect.stringContaining('NIST.800.53-EFSEncryptedCheck:'),
-          }),
-        }),
-      );
+  test('nist80053EFSEncrypted: Elastic File Systems are encrypted', () => {
+    const positive = new Stack();
+    Aspects.of(positive).add(new NIST80053Checks());
+    new FileSystem(positive, 'rEFS', {
+      vpc: new Vpc(positive, 'rVpc'),
+      encrypted: false,
     });
+    const messages = SynthUtils.synthesize(positive).messages;
+    expect(messages).toContainEqual(
+      expect.objectContaining({
+        entry: expect.objectContaining({
+          data: expect.stringContaining('NIST.800.53-EFSEncrypted:'),
+        }),
+      }),
+    );
+    const negative = new Stack();
+    Aspects.of(negative).add(new NIST80053Checks());
+    new FileSystem(negative, 'rEFS', {
+      vpc: new Vpc(negative, 'rVpc'),
+      encrypted: true,
+    });
+    const messages2 = SynthUtils.synthesize(negative).messages;
+    expect(messages2).not.toContainEqual(
+      expect.objectContaining({
+        entry: expect.objectContaining({
+          data: expect.stringContaining('NIST.800.53-EFSEncrypted:'),
+        }),
+      }),
+    );
+  });
 });
