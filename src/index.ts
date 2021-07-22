721cb9f07a07867326b7032c9a45efd4a6094faa
hendrann <hendrann@amazon.com>
2021-07-22 13:51:47 -0500

    renaming files and removing duplicate code in index file

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,3 @@ SPDX-License-Identifier: Apache-2.0
 export * from './AwsSolutions/aws-solutions';
 export * from './NIST-800-53/nist-800-53';
 export * from './common';
-export * from './NIST-800-53/nist-800-53';
