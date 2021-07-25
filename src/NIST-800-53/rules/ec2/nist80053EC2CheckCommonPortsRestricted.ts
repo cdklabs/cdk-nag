4bc8fd3bdd8208465b3929463a49669c47e730db
Alex Chang <alchanga@amazon.com>
2021-07-25 16:24:38 -0400

    better comments

--- src/NIST-800-53/rules/ec2/nist80053EC2CheckCommonPortsRestricted.ts
+++ src/NIST-800-53/rules/ec2/nist80053EC2CheckCommonPortsRestricted.ts
@@ -10,31 +10,11 @@ import { IConstruct, Stack } from '@aws-cdk/core';
 const blockedPorts=[20, 21, 3389, 3309, 3306, 4333];
 
 /**
- * EC2 instances have all common ports restricted - (Control IDs: CA-7(a)(b), SI-4(2), SI-4(a)(b)(c)).
+ * EC2 instances have all common ports restricted - (AC-4, CM-2, SC-7, SC-7(3)).
  * https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/default-custom-security-groups.html
  * @param node the CfnResource to check
  */
-function testPort (rule: CfnSecurityGroupIngress, portNum: Number): boolean {
-  //Is a port range specified?
-  if (rule.fromPort != undefined && rule.toPort != undefined) {
-    if ((rule.fromPort <= portNum && rule.toPort >= portNum) ||
-        (rule.fromPort == -1 || rule.toPort == -1) ||
-        rule.ipProtocol == '-1') {
-      return false;
-    }
-  } else {
-    if (rule.fromPort == portNum || rule.ipProtocol == '-1') {
-      return false;
-    }
-  }
-  return true;
-}
-
-/**
- * Security Groups do not allow for unrestricted SSH traffic - (Control IDs: AC-4, SC-7, SC-7(3))
- * @param node the CfnResource to check
- */
-export default function (node: IConstruct): boolean {
+ export default function (node: IConstruct): boolean {
   if (node instanceof CfnSecurityGroup) {
     const ingressRules = Stack.of(node).resolve(node.securityGroupIngress);
     if (ingressRules != undefined) {
@@ -57,3 +37,28 @@ export default function (node: IConstruct): boolean {
   }
   return true;
 }
+
+
+/**
+ * Helper function to identify if the given port number is unrestricted
+ * @param rule the CfnSecurityGroupIngress rule to check
+ * @param portNum the number of the port to check
+ */
+ function testPort (rule: CfnSecurityGroupIngress, portNum: Number): boolean {
+  //Is a port range specified?
+  if (rule.fromPort != undefined && rule.toPort != undefined) {
+    if ((rule.fromPort <= portNum && rule.toPort >= portNum) ||
+        (rule.fromPort == -1 || rule.toPort == -1) ||
+        rule.ipProtocol == '-1') {
+      return false;
+    }
+  } else {
+    if (rule.fromPort == portNum || rule.ipProtocol == '-1') {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
