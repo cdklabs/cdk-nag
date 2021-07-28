7581958bd73ff3a153d6a75c47bbdd5189b194ef
Alex Chang <alchanga@amazon.com>
2021-07-28 11:45:20 -0400

    implemented rest of ELB rules

---
+++ src/NIST-800-53/rules/elb/nist80053ELBDeletionProtectionEnabled.ts
@@ -0,0 +1,31 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { CfnLoadBalancer } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { IConstruct, Stack } from '@aws-cdk/core';
+
+/**
+ * ELBs are balanced across availability zones - (Control IDs: SC-5, CP-10)
+ * @param node the CfnResource to check
+ */
+export default function (node: IConstruct): boolean {
+  if (node instanceof CfnLoadBalancer) {
+    const attributes = Stack.of(node).resolve(node.loadBalancerAttributes);
+    if (attributes != undefined) {
+      if ("deletion_protection.enabled" in attributes){
+        if (attributes["deletion_protection.enabled"] != true){
+          return false;
+        }
+      }
+      else{
+        return false;
+      }
+    }
+    else{
+      return false;
+    }
+  }
+  return true;
+}
