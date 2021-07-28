28bfd9147acb3886cf5a78754c5ef1db3327596b
jen-rich <44377149+jen-rich@users.noreply.github.com>
2021-07-28 13:01:49 -0400

    add control id to rule
    Co-authored-by: Arun Donti <dontirun@gmail.com>

--- src/NIST-800-53/rules/redshift/nist80053RedshiftClusterConfiguration.ts
+++ src/NIST-800-53/rules/redshift/nist80053RedshiftClusterConfiguration.ts
@@ -6,7 +6,7 @@ import { CfnCluster } from '@aws-cdk/aws-redshift';
 import { CfnResource, Stack } from '@aws-cdk/core';
 
 /**
- * Redshift clusters have encryption and audit logging enabled
+ * Redshift clusters have encryption and audit logging enabled - (Control IDs: AC-2(4), AC-2(g), AU-2(a)(d), AU-3, AU-12(a)(c), SC-13)
  * @param node the CfnResource to check
  */
 export default function (node: CfnResource): boolean {
