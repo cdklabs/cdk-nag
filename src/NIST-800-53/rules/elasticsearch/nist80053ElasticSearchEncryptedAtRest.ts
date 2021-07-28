91b0bd7f7ad28cd8b0943c6092bd27f60c427984
Alex Chang <alchanga@amazon.com>
2021-07-28 03:14:11 -0400

    elasticsearch rules implemented

---
+++ src/NIST-800-53/rules/elasticsearch/nist80053ElasticSearchEncryptedAtRest.ts
@@ -0,0 +1,27 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { CfnDomain } from '@aws-cdk/aws-elasticsearch';
+import { IConstruct, Stack } from '@aws-cdk/core';
+
+/**
+ * Elasticsearch service domains have encryption at rest enabled - (Control IDs: SC-13, SC-28)
+ * @param node the CfnResource to check
+ */
+export default function (node: IConstruct): boolean {
+  if (node instanceof CfnDomain) {
+    //Is the encryption at rest property set?
+    const encryptionAtRestOptions = Stack.of(node).resolve(node.encryptionAtRestOptions);
+    if (encryptionAtRestOptions != undefined) {
+     if (encryptionAtRestOptions.Enabled == undefined || encryptionAtRestOptions.Enabled == false){
+      return false;
+     }
+    }
+    else{
+      return false;
+    }
+  }
+  return true;
+}
