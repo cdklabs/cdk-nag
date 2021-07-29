bbabfd6269bd7bed04e86620a5d8117f418548df
jen-rich <jarich150@gmail.com>
2021-07-29 13:31:22 -0400

    added rule

---
+++ src/NIST-800-53/rules/dynamodb/nist80053DynamoDBPITREnabled.ts
@@ -0,0 +1,21 @@
+import { CfnTable } from '@aws-cdk/aws-dynamodb'
+import { CfnResource, Stack } from '@aws-cdk/core';
+
+/**
+ * DynamoDB tables have Point-in-time Recovery enabled (Control IDs: CP-9(b), CP-10, SI-12)
+ * @param node the CfnResource to check
+ */
+
+ export default function (node: CfnResource): boolean {
+    if (node instanceof CfnTable) {
+      if (node.pointInTimeRecoverySpecification == undefined) {
+        return false;
+      }
+      const pitr = Stack.of(node).resolve(node.pointInTimeRecoverySpecification);
+      const enabled = Stack.of(node).resolve(pitr.pointInTimeRecoveryEnabled);
+      if (!enabled) {
+        return false;
+      }
+    }
+    return true;
+  }
