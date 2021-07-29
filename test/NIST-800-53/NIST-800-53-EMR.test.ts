c0043e9dae3fb6a597ae0fa33e5ad24e8e248640
jen-rich <jarich150@gmail.com>
2021-07-29 11:51:25 -0400

    edit test

---
+++ test/NIST-800-53/NIST-800-53-EMR.test.ts
@@ -0,0 +1,50 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { SynthUtils } from '@aws-cdk/assert';
+import { CfnCluster } from '@aws-cdk/aws-emr';
+import { Aspects, Stack } from '@aws-cdk/core';
+import { NIST80053Checks } from '../../src';
+
+describe ("Amazon EMR", () => {
+    test('NIST.800.53-EMRKerberosEnabled: EMR clusters have Kerberos enabled', () => {
+        const positive = new Stack();
+        Aspects.of(positive).add(new NIST80053Checks());
+        new CfnCluster(positive, 'rEmrCluster', {
+          instances: {},
+          jobFlowRole: ' EMR_EC2_DefaultRole',
+          name: 'foo',
+          serviceRole: 'bar',
+        });
+        const messages = SynthUtils.synthesize(positive).messages;
+        expect(messages).toContainEqual(
+          expect.objectContaining({
+            entry: expect.objectContaining({
+              data: expect.stringContaining('NIST.800.53-EMRKerberosEnabled:'),
+            }),
+          }),
+        );
+  
+        const negative = new Stack();
+        Aspects.of(negative).add(new NIST80053Checks());
+        new CfnCluster(negative, 'rEmrCluster', {
+          instances: {},
+          jobFlowRole: ' EMR_EC2_DefaultRole',
+          name: 'foo',
+          serviceRole: 'bar',
+          kerberosAttributes: {
+            kdcAdminPassword: 'baz',
+            realm: 'qux',
+          },
+        });
+        const messages2 = SynthUtils.synthesize(negative).messages;
+        expect(messages2).not.toContainEqual(
+          expect.objectContaining({
+            entry: expect.objectContaining({
+              data: expect.stringContaining('NIST.800.53-EMRKerberosEnabled:'),
+            }),
+          }),
+        );
+    });
+}); 
