12f9bbb0b915d8bd415a89b53927836e60caac0a
hendrann <hendrann@amazon.com>
2021-08-04 17:02:27 -0500

    fixing RDS logging rule, adding tests, and updating RULES.md

--- src/NIST-800-53/rules/rds/nist80053RDSLoggingEnabled.ts
+++ src/NIST-800-53/rules/rds/nist80053RDSLoggingEnabled.ts
@@ -2,57 +2,56 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-import { CfnDBInstance, DatabaseInstanceEngine } from '@aws-cdk/aws-rds';
+import { CfnDBInstance } from '@aws-cdk/aws-rds';
 import { IConstruct, Stack } from '@aws-cdk/core';
 
 /**
- * RDS Database Instances are configured to export all possible log types to Cloudwatch - (Control IDs: AC-2(1), AC-2(j), AC-3, AC-6)
+ * RDS Database Instances are configured to export all possible log types to Cloudwatch - (Control IDs: AC-2(4), AC-2(g), AU-2(a)(d), AU-3, AU-12(a)(c))
  * @param node the CfnResource to check
  */
 export default function (node: IConstruct): boolean {
   if (node instanceof CfnDBInstance) {    
-    const dbType = Stack.of(node).resolve(node.engine);
-    const dbLogs = Stack.of(node).resolve(node.enableCloudwatchLogsExports);
+    const dbType = JSON.stringify(Stack.of(node).resolve(node.engine));
+    const dbLogs = JSON.stringify(Stack.of(node).resolve(node.enableCloudwatchLogsExports));
     if (dbLogs == undefined){
         return false;
     }
-    const exportString = JSON.stringify(dbLogs);
-    if (dbType == (DatabaseInstanceEngine.mariaDb || DatabaseInstanceEngine.mysql)){
-        if (
-        (exportString.search('audit') < 0) ||
-        (exportString.search('error') < 0) ||
-        (exportString.search('general') < 0) ||
-        (exportString.search('slowquery') < 0)
-        )
+
+    if (dbType.includes('mariadb') || (dbType.includes('mysql'))){
+        if (!(
+        (dbLogs.includes('audit')) &&
+        (dbLogs.includes('error')) &&
+        (dbLogs.includes('general')) &&
+        (dbLogs.includes('slowquery'))
+        ))
         return false;
     }
-    if (dbType == DatabaseInstanceEngine.postgres){
-        if (
-        (exportString.search('postgresql') < 0) ||
-        (exportString.search('upgrade') < 0)
-        )
+
+    if (dbType.includes('postgres')){
+        if (!(
+        (dbLogs.includes('postgresql')) &&
+        (dbLogs.includes('upgrade'))
+        ))
         return false;
     }
-    if (dbType == (DatabaseInstanceEngine.oracleEe || DatabaseInstanceEngine.oracleSe2)){
-        if (
-        (exportString.search('audit') < 0) ||
-        (exportString.search('alert') < 0) ||
-        (exportString.search('listener') < 0) ||
-        (exportString.search('oemagent') < 0) ||
-        (exportString.search('trace') < 0)
-        )
+    
+    if (dbType.includes('oracle')){
+        console.log("here");
+        if (!(
+        (dbLogs.includes('audit')) &&
+        (dbLogs.includes('alert')) &&
+        (dbLogs.includes('listener')) &&
+        (dbLogs.includes('oemagent')) &&
+        (dbLogs.includes('trace'))
+        ))
         return false;
     }
-    if (dbType == (
-        DatabaseInstanceEngine.sqlServerEe || 
-        DatabaseInstanceEngine.sqlServerEx || 
-        DatabaseInstanceEngine.sqlServerSe || 
-        DatabaseInstanceEngine.sqlServerWeb)
-        ){
-        if (
-        (exportString.search('agent') < 0) ||
-        (exportString.search('error') < 0)
-        )
+    
+    if (dbType.includes('sqlserver')){
+        if (!(
+        (dbLogs.includes('agent')) &&
+        (dbLogs.includes('error'))
+        ))
         return false;
     }
   }
--- test/NIST-800-53/NIST-800-53-RDS.test.ts
+++ test/NIST-800-53/NIST-800-53-RDS.test.ts
@@ -6,6 +6,10 @@ import { SynthUtils } from '@aws-cdk/assert';
 import { DatabaseInstance, 
     DatabaseInstanceEngine,
     MariaDbEngineVersion,
+    PostgresEngineVersion,
+    SqlServerEngineVersion,
+    MysqlEngineVersion,
+    OracleEngineVersion
  } from '@aws-cdk/aws-rds';
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { Aspects, Stack } from '@aws-cdk/core';
@@ -17,7 +21,6 @@ describe('NIST 800-53 Compliance Checks', () => {
 
       const nonCompliant = new Stack();
       Aspects.of(nonCompliant).add(new NIST80053Checks());
-      
       new DatabaseInstance(nonCompliant, 'rDbInstance', {
         engine: DatabaseInstanceEngine.mariaDb({
           version: MariaDbEngineVersion.VER_10_2,
@@ -25,7 +28,6 @@ describe('NIST 800-53 Compliance Checks', () => {
         port: 5432,
         vpc: new Vpc(nonCompliant, 'rVpc'),
       });
-
       const messages = SynthUtils.synthesize(nonCompliant).messages;
       expect(messages).toContainEqual(
         expect.objectContaining({
@@ -35,9 +37,82 @@ describe('NIST 800-53 Compliance Checks', () => {
         }),
       );
 
+      const nonCompliant2 = new Stack();
+      Aspects.of(nonCompliant2).add(new NIST80053Checks());
+      new DatabaseInstance(nonCompliant2, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.postgres({
+          version: PostgresEngineVersion.VER_10,
+        }),
+        port: 5432,
+        vpc: new Vpc(nonCompliant2, 'rVpc'),
+      });
+      const messages2 = SynthUtils.synthesize(nonCompliant2).messages;
+      expect(messages2).toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const nonCompliant3 = new Stack();
+      Aspects.of(nonCompliant3).add(new NIST80053Checks());
+      new DatabaseInstance(nonCompliant3, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.sqlServerWeb({
+          version: SqlServerEngineVersion.VER_11,
+        }),
+        port: 5432,
+        vpc: new Vpc(nonCompliant3, 'rVpc'),
+        cloudwatchLogsExports: ['error']
+      });
+      const messages3 = SynthUtils.synthesize(nonCompliant3).messages;
+      expect(messages3).toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const nonCompliant4 = new Stack();
+      Aspects.of(nonCompliant4).add(new NIST80053Checks());
+      new DatabaseInstance(nonCompliant4, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.mysql({
+          version: MysqlEngineVersion.VER_8_0_25,
+        }),
+        port: 5432,
+        vpc: new Vpc(nonCompliant4, 'rVpc'),
+      });
+      const messages4 = SynthUtils.synthesize(nonCompliant4).messages;
+      expect(messages4).toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const nonCompliant5 = new Stack();
+      Aspects.of(nonCompliant5).add(new NIST80053Checks());
+      new DatabaseInstance(nonCompliant5, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.oracleEe({
+          version: OracleEngineVersion.VER_19_0_0_0_2021_04_R1,
+        }),
+        port: 5432,
+        vpc: new Vpc(nonCompliant5, 'rVpc'),
+        cloudwatchLogsExports: ['trace', 'listener']
+      });
+      const messages5 = SynthUtils.synthesize(nonCompliant5).messages;
+      expect(messages5).toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
       const compliant = new Stack();
       Aspects.of(compliant).add(new NIST80053Checks());
-
       new DatabaseInstance(compliant, 'rDbInstance', {
         engine: DatabaseInstanceEngine.mariaDb({
           version: MariaDbEngineVersion.VER_10_2,
@@ -46,22 +121,95 @@ describe('NIST 800-53 Compliance Checks', () => {
         vpc: new Vpc(compliant, 'rVpc'),
         cloudwatchLogsExports: ['audit', 'error', 'general', 'slowquery']
       });
-
-      const messages2 = SynthUtils.synthesize(compliant).messages;
-      expect(messages2).not.toContainEqual(
+      const messages6 = SynthUtils.synthesize(compliant).messages;
+      expect(messages6).not.toContainEqual(
         expect.objectContaining({
           entry: expect.objectContaining({
             data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
           }),
         }),
       );
-    });
 
-    const passiveCompliant = new Stack();
+      const compliant2 = new Stack();
+      Aspects.of(compliant2).add(new NIST80053Checks());
+      new DatabaseInstance(compliant2, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.postgres({
+          version: PostgresEngineVersion.VER_11,
+        }),
+        port: 5432,
+        vpc: new Vpc(compliant2, 'rVpc'),
+        cloudwatchLogsExports: ['postgresql', 'upgrade']
+      });
+      const messages7 = SynthUtils.synthesize(compliant2).messages;
+      expect(messages7).not.toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const compliant3 = new Stack();
+      Aspects.of(compliant3).add(new NIST80053Checks());
+      new DatabaseInstance(compliant3, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.sqlServerEe({
+          version: SqlServerEngineVersion.VER_11,
+        }),
+        port: 5432,
+        vpc: new Vpc(compliant3, 'rVpc'),
+        cloudwatchLogsExports: ['agent', 'error']
+      });
+      const messages8 = SynthUtils.synthesize(compliant3).messages;
+      expect(messages8).not.toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const compliant4 = new Stack();
+      Aspects.of(compliant4).add(new NIST80053Checks());
+      new DatabaseInstance(compliant4, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.mysql({
+          version: MysqlEngineVersion.VER_8_0_25,
+        }),
+        port: 5432,
+        vpc: new Vpc(compliant4, 'rVpc'),
+        cloudwatchLogsExports: ['audit', 'error', 'general', 'slowquery']
+      });
+      const messages9 = SynthUtils.synthesize(compliant4).messages;
+      expect(messages9).not.toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const compliant5 = new Stack();
+      Aspects.of(compliant5).add(new NIST80053Checks());
+      new DatabaseInstance(compliant5, 'rDbInstance', {
+        engine: DatabaseInstanceEngine.oracleSe2({
+          version: OracleEngineVersion.VER_19_0_0_0_2021_04_R1,
+        }),
+        port: 5432,
+        vpc: new Vpc(compliant5, 'rVpc'),
+        cloudwatchLogsExports: ['trace', 'listener', 'audit', 'alert', 'oemagent']
+      });
+      const messages10 = SynthUtils.synthesize(compliant5).messages;
+      expect(messages10).not.toContainEqual(
+        expect.objectContaining({
+          entry: expect.objectContaining({
+            data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
+          }),
+        }),
+      );
+
+      const passiveCompliant = new Stack();
       Aspects.of(passiveCompliant).add(new NIST80053Checks());
-      
-      const messages3 = SynthUtils.synthesize(passiveCompliant).messages;
-      expect(messages3).not.toContainEqual(
+      const messages11 = SynthUtils.synthesize(passiveCompliant).messages;
+      expect(messages11).not.toContainEqual(
         expect.objectContaining({
           entry: expect.objectContaining({
             data: expect.stringContaining('NIST.800.53-RDSLoggingEnabled:'),
@@ -69,5 +217,6 @@ describe('NIST 800-53 Compliance Checks', () => {
         }),
       );
 
+    });
   });
 });
