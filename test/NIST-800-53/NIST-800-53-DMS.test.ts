9aafb3638a3e53998bba94cf1106612f73f47517
jen-rich <jarich150@gmail.com>
2021-08-05 10:09:10 -0400

    changes to mistakes in file

--- test/NIST-800-53/NIST-800-53-DMS.test.ts
+++ test/NIST-800-53/NIST-800-53-DMS.test.ts
@@ -4,41 +4,39 @@ SPDX-License-Identifier: Apache-2.0
 */
 
 import { SynthUtils } from '@aws-cdk/assert';
-import {
-  CfnReplicationInstance
-} from '@aws-cdk/aws-dms';
+import { CfnReplicationInstance } from '@aws-cdk/aws-dms';
 import { Aspects, Stack } from '@aws-cdk/core';
 import { NIST80053Checks } from '../../src';
-  describe('AWS Database Migration Service (AWS DMS)', () => {
-    test('nist80053DMSReplication: - DMS replication is not public - (Control ID: AC-4)', () => {
-      const positive = new Stack();
-      Aspects.of(positive).add(new NIST80053Checks());
-      new CfnReplicationInstance(positive, 'rInstance', {
-        replicationInstanceClass: 'dms.t2.micro',
-        publiclyAccessible: true,
-      });
-      const messages = SynthUtils.synthesize(positive).messages;
-      expect(messages).toContainEqual(
-        expect.objectContaining({
-          entry: expect.objectContaining({
-            data: expect.stringContaining('NIST.800.53-DMSReplicationNotPublic:'),
-          }),
-        })
-      );
-
-      const negative = new Stack();
-      Aspects.of(negative).add(new NIST80053Checks());
-      new CfnReplicationInstance(positive, 'rInstance', {
-        replicationInstanceClass: 'dms.t2.micro',
-        publiclyAccessible: false,
-      });
-      const messages2 = SynthUtils.synthesize(positive).messages;
-      expect(messages2).toContainEqual(
-        expect.objectContaining({
-          entry: expect.objectContaining({
-            data: expect.stringContaining('NIST.800.53-DMSReplicationNotPublic:'),
-          }),
-        })
-      );
+describe('AWS Database Migration Service (AWS DMS)', () => {
+  test('nist80053DMSReplication: - DMS replication is not public - (Control ID: AC-4)', () => {
+    const positive = new Stack();
+    Aspects.of(positive).add(new NIST80053Checks());
+    new CfnReplicationInstance(positive, 'rInstance', {
+      replicationInstanceClass: 'dms.t2.micro',
+      publiclyAccessible: true,
     });
+    const messages = SynthUtils.synthesize(positive).messages;
+    expect(messages).toContainEqual(
+      expect.objectContaining({
+        entry: expect.objectContaining({
+          data: expect.stringContaining('NIST.800.53-DMSReplicationNotPublic:'),
+        }),
+      })
+    );
+
+    const negative = new Stack();
+    Aspects.of(negative).add(new NIST80053Checks());
+    new CfnReplicationInstance(negative, 'rInstance', {
+      replicationInstanceClass: 'dms.t2.micro',
+      publiclyAccessible: false,
+    });
+    const messages2 = SynthUtils.synthesize(negative).messages;
+    expect(messages2).not.toContainEqual(
+      expect.objectContaining({
+        entry: expect.objectContaining({
+          data: expect.stringContaining('NIST.800.53-DMSReplicationNotPublic:'),
+        }),
+      })
+    );
+  });
 });
