075307803560afbff16fdbde68e460edd3559452
hendrann <hendrann@amazon.com>
2021-08-06 16:06:58 -0500

    pulling DMS rules from main

---
+++ src/NIST-800-53/rules/dms/nist80053DMSReplicationNotPublic.ts
@@ -0,0 +1,20 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { CfnReplicationInstance } from '@aws-cdk/aws-dms';
+import { CfnResource, Stack } from '@aws-cdk/core';
+
+/**
+ * DMS replication instances are not public - (Control ID: AC-4)
+ * @param node the CfnResource to check
+ */
+export default function (node: CfnResource): boolean {
+  if (node instanceof CfnReplicationInstance) {
+    const publicAccess = Stack.of(node).resolve(node.publiclyAccessible);
+    if (!(publicAccess === false)) {
+      return false;
+    }
+  }
+  return true;
+}
