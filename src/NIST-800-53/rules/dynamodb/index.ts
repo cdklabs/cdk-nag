f4bea9c396317cc6bde3b83c2b2b2dc731605550
Arun Donti <dontirun@gmail.com>
2021-08-27 13:04:41 -0400

    ci: pre-commit hook to populate missing legal text on files

--- src/NIST-800-53/rules/dynamodb/index.ts
+++ src/NIST-800-53/rules/dynamodb/index.ts
@@ -1,1 +1,5 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
 export { default as nist80053DynamoDBPITREnabled } from './nist80053DynamoDBPITREnabled';
