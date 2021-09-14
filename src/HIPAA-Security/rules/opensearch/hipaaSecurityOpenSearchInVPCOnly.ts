f56e57de2fd5d094f03effbf1a1c4659c345d0c4
Arun Donti <dontirun@gmail.com>
2021-09-14 18:45:35 -0400

    feat(HIPAA Security): OpenSearch checks

---
+++ src/HIPAA-Security/rules/opensearch/hipaaSecurityOpenSearchInVPCOnly.ts
@@ -0,0 +1,28 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { CfnDomain } from '@aws-cdk/aws-elasticsearch';
+import { IConstruct, Stack } from '@aws-cdk/core';
+
+/**
+ * OpenSearch Service domains are within VPCs - (Control IDs: 164.308(a)(3)(i), 164.308(a)(4)(ii)(A), 164.308(a)(4)(ii)(C), 164.312(a)(1), 164.312(e)(1))
+ * @param node the CfnResource to check
+ */
+export default function (node: IConstruct): boolean {
+  if (node instanceof CfnDomain) {
+    const vpcOptions = Stack.of(node).resolve(node.vpcOptions);
+    if (vpcOptions != undefined) {
+      if (
+        vpcOptions.subnetIds == undefined ||
+        vpcOptions.subnetIds.length == 0
+      ) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
