39c2965582c297ae1a16b6ee157d91afa3a5eec4
Arun Donti <dontirun@gmail.com>
2021-09-22 17:18:24 -0400

    feat(HIPAA Security): SageMaker Checks

---
+++ src/HIPAA-Security/rules/sagemaker/hipaaSecuritySageMakerNotebookNoDirectInternetAccess.ts
@@ -0,0 +1,26 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { CfnNotebookInstance } from '@aws-cdk/aws-sagemaker';
+import { IConstruct, Stack } from '@aws-cdk/core';
+
+/**
+ * SageMaker notebook instances have direct internet access disabled - (Control IDs: 164.308(a)(3)(i), 164.308(a)(4)(ii)(A), 164.308(a)(4)(ii)(C), 164.312(a)(1), 164.312(e)(1))
+ * @param node the CfnResource to check
+ */
+export default function (node: IConstruct): boolean {
+  if (node instanceof CfnNotebookInstance) {
+    const directInternetAccess = Stack.of(node).resolve(
+      node.directInternetAccess
+    );
+    if (
+      directInternetAccess == undefined ||
+      directInternetAccess != 'Disabled'
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
