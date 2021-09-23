c6946e903db66b6bf5441e8aa98ebf4d8f19a6a4
Arun Donti <dontirun@gmail.com>
2021-09-22 20:54:48 -0400

    feat(HIPAA Security): Lambda Checks (#362)
    Closes #202 
    Closes #203 
    Closes #205 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- src/HIPAA-Security/rules/lambda/index.ts
+++ src/HIPAA-Security/rules/lambda/index.ts
@@ -2,3 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
+
+export { default as hipaaSecurityLambdaConcurrency } from './hipaaSecurityLambdaConcurrency';
+export { default as hipaaSecurityLambdaDlq } from './hipaaSecurityLambdaDlq';
+export { default as hipaaSecurityLambdaInsideVPC } from './hipaaSecurityLambdaInsideVPC';
