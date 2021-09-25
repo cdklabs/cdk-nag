ced1a0ac3c2ebb1bfa441211df2e65e11291b4d5
Arun Donti <dontirun@gmail.com>
2021-09-25 04:20:44 -0400

    feat(HIPAA Security): S3 Checks (#373)
    Closes #269
    Closes #270
    Closes #272
    Closes #273
    Closes #274
    Closes #275
    Closes #277
    Closes #278
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ src/HIPAA-Security/rules/s3/hipaaSecurityS3BucketServerSideEncryptionEnabled.ts
@@ -0,0 +1,40 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { CfnBucket } from '@aws-cdk/aws-s3';
+import { CfnResource, Stack } from '@aws-cdk/core';
+
+/**
+ * S3 Buckets have default server-side encryption enabled - (Control IDs: 164.312(a)(2)(iv), 164.312(c)(2), 164.312(e)(2)(ii))
+ * @param node the CfnResource to check
+ */
+export default function (node: CfnResource): boolean {
+  if (node instanceof CfnBucket) {
+    if (node.bucketEncryption == undefined) {
+      return false;
+    }
+    const encryption = Stack.of(node).resolve(node.bucketEncryption);
+
+    if (encryption.serverSideEncryptionConfiguration == undefined) {
+      return false;
+    }
+
+    const sse = Stack.of(node).resolve(
+      encryption.serverSideEncryptionConfiguration
+    );
+    for (const rule of sse) {
+      const defaultEncryption = Stack.of(node).resolve(
+        rule.serverSideEncryptionByDefault
+      );
+      if (
+        defaultEncryption == undefined ||
+        (defaultEncryption.sseAlgorithm.toLowerCase() != 'aes256' &&
+          defaultEncryption.sseAlgorithm.toLowerCase() != 'aws:kms')
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
---
+++ src/HIPAA-Security/rules/s3/hipaaSecurityS3DefaultEncryptionKMS.ts
@@ -0,0 +1,39 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { CfnBucket } from '@aws-cdk/aws-s3';
+import { CfnResource, Stack } from '@aws-cdk/core';
+
+/**
+ * S3 Buckets are encrypted with a KMS Key by default - (Control IDs: 164.312(a)(2)(iv), 164.312(e)(2)(ii))
+ * @param node the CfnResource to check
+ */
+export default function (node: CfnResource): boolean {
+  if (node instanceof CfnBucket) {
+    if (node.bucketEncryption == undefined) {
+      return false;
+    }
+    const encryption = Stack.of(node).resolve(node.bucketEncryption);
+
+    if (encryption.serverSideEncryptionConfiguration == undefined) {
+      return false;
+    }
+
+    const sse = Stack.of(node).resolve(
+      encryption.serverSideEncryptionConfiguration
+    );
+    for (const rule of sse) {
+      const defaultEncryption = Stack.of(node).resolve(
+        rule.serverSideEncryptionByDefault
+      );
+      if (
+        defaultEncryption == undefined ||
+        defaultEncryption.sseAlgorithm.toLowerCase() != 'aws:kms'
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
