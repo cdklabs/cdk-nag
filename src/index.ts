fb0e6f8a074cfe30dadba6fea099ee23c211cb37
Arun Donti <dontirun@gmail.com>
2021-10-26 14:53:08 -0400

    refactor(PCI DSS 3.2.1): reordering exports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ SPDX-License-Identifier: Apache-2.0
 */
 export * from './AwsSolutions/aws-solutions';
 export * from './HIPAA-Security/hipaa-security';
-export * from './PCI-DSS-3.2.1/pci-dss-321';
 export * from './NIST-800-53-R4/nist-800-53-r4';
 export * from './NIST-800-53-R5/nist-800-53-r5';
+export * from './PCI-DSS-3.2.1/pci-dss-321';
 export * from './nag-pack';
 export * from './nag-suppressions';
