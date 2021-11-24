da5500d90d56e0c111e66c24b36d8c5369b1f131
Arun Donti <dontirun@gmail.com>
2021-11-24 10:58:18 -0500

    feat: export rules as package

---
+++ src/rules/index.ts
@@ -0,0 +1,45 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+export * as apigw from './apigw';
+export * as appsync from './appsync';
+export * as athena from './athena';
+export * as autoscaling from './autoscaling';
+export * as cloud9 from './cloud9';
+export * as cloudfront from './cloudfront';
+export * as cloudtrail from './cloudtrail';
+export * as cloudwatch from './cloudwatch';
+export * as codebuild from './codebuild';
+export * as cognito from './cognito';
+export * as dms from './dms';
+export * as documentdb from './documentdb';
+export * as dynamodb from './dynamodb';
+export * as ec2 from './ec2';
+export * as ecr from './ecr';
+export * as ecs from './ecs';
+export * as efs from './efs';
+export * as elasticache from './elasticache';
+export * as elasticbeanstalk from './elasticbeanstalk';
+export * as elb from './elb';
+export * as emr from './emr';
+export * as iam from './iam';
+export * as kinesis from './kinesis';
+export * as kms from './kms';
+export * as lambda from './lambda';
+export * as mediastore from './mediastore';
+export * as msk from './msk';
+export * as neptune from './neptune';
+export * as opensearch from './opensearch';
+export * as quicksight from './quicksight';
+export * as rds from './rds';
+export * as redshift from './redshift';
+export * as s3 from './s3';
+export * as sagemaker from './sagemaker';
+export * as secretsmanager from './secretsmanager';
+export * as sns from './sns';
+export * as sqs from './sqs';
+export * as stepfunctions from './stepfunctions';
+export * as timestream from './timestream';
+export * as vpc from './vpc';
+export * as waf from './waf';
