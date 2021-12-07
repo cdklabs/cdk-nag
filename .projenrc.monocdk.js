c1294204efa31b4a0d4592f2de9f22483298d3f7
Arun Donti <dontirun@gmail.com>
2021-12-07 17:08:01 -0500

    chore(deps): upgrade dependencies and projen api (#515)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- .projenrc.monocdk.js
+++ .projenrc.monocdk.js
@@ -2,8 +2,8 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-const { AwsCdkConstructLibrary } = require('projen');
-const project = new AwsCdkConstructLibrary({
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Arun Donti',
   authorAddress: 'donti@amazon.com',
   cdkVersion: '1.123.0',
@@ -23,7 +23,5 @@ const project = new AwsCdkConstructLibrary({
 project.package.addField('resolutions', {
   'ansi-regex': '^5.0.1',
   'json-schema': '^0.4.0',
-  'jest-environment-jsdom':
-    'https://registry.yarnpkg.com/@favware/skip-dependency/-/skip-dependency-1.1.3.tgz',
 });
 project.synth();
