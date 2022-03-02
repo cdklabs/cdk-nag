fcc79d2958b41bfa0e81d851010cf12848d3bc49
Arun Donti <dontirun@gmail.com>
2022-03-02 17:19:07 -0500

    chore: Maven and NuGet publishing (#682)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- .projenrc.monocdk.js
+++ .projenrc.monocdk.js
@@ -2,7 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-const { awscdk } = require('projen');
+const { awscdk, DependencyType } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Arun Donti',
   authorAddress: 'donti@amazon.com',
@@ -12,14 +12,28 @@ const project = new awscdk.AwsCdkConstructLibrary({
   description:
     'Check CDK applications for best practices using a combination on available rule packs.',
   repositoryUrl: 'https://github.com/cdklabs/cdk-nag.git',
-
   cdkDependencies: ['monocdk'],
   cdkTestDependencies: ['@monocdk-experiment/assert'],
+  devDeps: ['constructs@^3.2.27'],
   publishToPypi: {
     distName: 'monocdk-nag',
     module: 'monocdk_nag',
   },
+  publishToNuget: {
+    packageId: 'Cdklabs.MonocdkNag',
+    dotNetNamespace: 'Cdklabs.MonocdkNag',
+  },
+  publishToMaven: {
+    mavenGroupId: 'io.github.cdklabs',
+    javaPackage: 'io.github.cdklabs.monocdknag',
+    mavenArtifactId: 'monocdknag',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
 });
+project.deps.removeDependency('@aws-cdk/core', DependencyType.PEER);
+project.deps.removeDependency('@aws-cdk/core', DependencyType.RUNTIME);
+project.deps.removeDependency('@aws-cdk/assert', DependencyType.TEST);
+project.deps.removeDependency('@aws-cdk/assertions', DependencyType.TEST);
 project.package.addField('resolutions', {
   'ansi-regex': '^5.0.1',
   'json-schema': '^0.4.0',
