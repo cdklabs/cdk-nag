3ebc9b2b8c180f3e38bedbd896c41407fc5462ea
Arun Donti <dontirun@gmail.com>
2022-03-03 16:07:18 -0500

    chore: update imports for v2

--- test/rules/S3.test.ts
+++ test/rules/S3.test.ts
@@ -2,7 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-import { OriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
+import { OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
 import {
   AnyPrincipal,
   ArnPrincipal,
