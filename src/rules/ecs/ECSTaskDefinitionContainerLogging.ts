a0cae3bb627e5cad9d58755f27da4990171b9bc8
Arun Donti <dontirun@gmail.com>
2022-03-31 12:31:42 -0400

    fix: ECSTaskDefinitionContainerLogging implementation (#749)
    * fix: ECSTaskDefinitionContainerLogging implementation
    
    * docs: updating rule verbiage

--- src/rules/ecs/ECSTaskDefinitionContainerLogging.ts
+++ src/rules/ecs/ECSTaskDefinitionContainerLogging.ts
@@ -4,31 +4,30 @@ SPDX-License-Identifier: Apache-2.0
 */
 import { parse } from 'path';
 import { CfnResource, Stack } from 'aws-cdk-lib';
-import { CfnCluster } from 'aws-cdk-lib/aws-ecs';
+import { CfnTaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { NagRuleCompliance } from '../../nag-rules';
 
 /**
- * ECS Task Definitions have awslogs logging enabled at the minimum
+ * Containers in ECS Task Definitions have logging enabled
  * @param node the CfnResource to check
  */
 export default Object.defineProperty(
   (node: CfnResource): NagRuleCompliance => {
-    if (node instanceof CfnCluster) {
-      if (node.configuration == undefined) {
-        return NagRuleCompliance.NON_COMPLIANT;
-      }
-      const configuration = Stack.of(node).resolve(node.configuration);
-      if (configuration.executeCommandConfiguration == undefined) {
-        return NagRuleCompliance.NON_COMPLIANT;
-      }
-      const executeCommandConfiguration = Stack.of(node).resolve(
-        configuration.executeCommandConfiguration
+    if (node instanceof CfnTaskDefinition) {
+      const containerDefinitions = Stack.of(node).resolve(
+        node.containerDefinitions
       );
-      if (
-        executeCommandConfiguration.logging == undefined ||
-        executeCommandConfiguration.logging == 'NONE'
-      ) {
-        return NagRuleCompliance.NON_COMPLIANT;
+      if (containerDefinitions !== undefined) {
+        for (const containerDefinition of containerDefinitions) {
+          const resolvedDefinition =
+            Stack.of(node).resolve(containerDefinition);
+          const logConfiguration = Stack.of(node).resolve(
+            resolvedDefinition.logConfiguration
+          );
+          if (logConfiguration?.logDriver === undefined) {
+            return NagRuleCompliance.NON_COMPLIANT;
+          }
+        }
       }
       return NagRuleCompliance.COMPLIANT;
     } else {
