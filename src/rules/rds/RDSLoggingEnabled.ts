436245848165fd9743be0a3288f7f716da17067c
Arun Donti <dontirun@gmail.com>
2022-04-04 17:24:47 -0400

    fix: RDSLoggingEnabled (#760)

--- src/rules/rds/RDSLoggingEnabled.ts
+++ src/rules/rds/RDSLoggingEnabled.ts
@@ -3,7 +3,7 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 import { parse } from 'path';
-import { CfnDBInstance } from '@aws-cdk/aws-rds';
+import { CfnDBCluster, CfnDBInstance } from '@aws-cdk/aws-rds';
 import { CfnResource, Stack } from '@aws-cdk/core';
 import {
   NagRuleCompliance,
@@ -13,7 +13,7 @@ import {
 } from '../../nag-rules';
 
 /**
- * RDS DB instances are configured to export all possible log types to CloudWatch
+ * Non-Aurora RDS DB instances and Aurora clusters are configured to export all possible log types to CloudWatch
  * @param node the CfnResource to check
  */
 export default Object.defineProperty(
@@ -23,14 +23,38 @@ export default Object.defineProperty(
       const exports =
         Stack.of(node).resolve(node.enableCloudwatchLogsExports) ?? [];
       const needed: string[] = [];
-      if (dbType.includes('mariadb') || dbType.includes('mysql')) {
+      if (!dbType.includes('aurora')) {
+        if (dbType.includes('mariadb') || dbType.includes('mysql')) {
+          needed.push('audit', 'error', 'general', 'slowquery');
+        } else if (dbType.includes('postgres')) {
+          needed.push('postgresql', 'upgrade');
+        } else if (dbType.includes('oracle')) {
+          needed.push('audit', 'alert', 'listener', 'oemagent', 'trace');
+        } else if (dbType.includes('sqlserver')) {
+          needed.push('agent', 'error');
+        }
+        const findings: NagRuleFindings = needed
+          .filter((log) => !exports.includes(log))
+          .map((log) => `LogExport::${log}`);
+        return findings.length ? findings : NagRuleCompliance.COMPLIANT;
+      } else {
+        return NagRuleCompliance.NOT_APPLICABLE;
+      }
+    } else if (node instanceof CfnDBCluster) {
+      const engine = NagRules.resolveIfPrimitive(
+        node,
+        node.engine
+      ).toLowerCase();
+      const needed: string[] = [];
+      const exports =
+        Stack.of(node).resolve(node.enableCloudwatchLogsExports) ?? [];
+      if (
+        engine.toLowerCase() === 'aurora' ||
+        engine.toLowerCase() === 'aurora-mysql'
+      ) {
         needed.push('audit', 'error', 'general', 'slowquery');
-      } else if (dbType.includes('postgres')) {
-        needed.push('postgresql', 'upgrade');
-      } else if (dbType.includes('oracle')) {
-        needed.push('audit', 'alert', 'listener', 'oemagent', 'trace');
-      } else if (dbType.includes('sqlserver')) {
-        needed.push('agent', 'error');
+      } else if (engine.toLowerCase() === 'aurora-postgresql') {
+        needed.push('postgresql');
       }
       const findings: NagRuleFindings = needed
         .filter((log) => !exports.includes(log))
