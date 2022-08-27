fe32db6b946ee8d9f46f1a573ec57b0d44ca2e58
Arun Donti <dontirun@gmail.com>
2022-08-27 09:11:00 -0400

    feat: Go support

--- .projenrc.js
+++ .projenrc.js
@@ -29,6 +29,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
     mavenArtifactId: 'cdknag',
     mavenEndpoint: 'https://s01.oss.sonatype.org',
   },
+  publishToGo: {
+    moduleName: 'github.com/cdklabs/cdk-nag-go',
+    githubUseSsh: true,
+  },
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
   autoApproveOptions: {
     allowedUsernames: ['cdklabs-automation'],
