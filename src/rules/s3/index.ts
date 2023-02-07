9a2a9c9b75500c0b4e8ef0e3ff9e8e006c97255e
clueleaf <10379303+clueleaf@users.noreply.github.com>
2023-02-08 08:59:58 +0900

    feat: remove S3BucketServerSideEncryptionEnabled rule (#1191)
    Fixes #1190
    S3BucketServerSideEncryptionEnabled rule is removed since S3 encrypts all new objects by default now.
    https://aws.amazon.com/blogs/aws/amazon-s3-encrypts-new-objects-by-default/

--- src/rules/s3/index.ts
+++ src/rules/s3/index.ts
@@ -8,7 +8,6 @@ export { default as S3BucketLoggingEnabled } from './S3BucketLoggingEnabled';
 export { default as S3BucketPublicReadProhibited } from './S3BucketPublicReadProhibited';
 export { default as S3BucketPublicWriteProhibited } from './S3BucketPublicWriteProhibited';
 export { default as S3BucketReplicationEnabled } from './S3BucketReplicationEnabled';
-export { default as S3BucketServerSideEncryptionEnabled } from './S3BucketServerSideEncryptionEnabled';
 export { default as S3BucketSSLRequestsOnly } from './S3BucketSSLRequestsOnly';
 export { default as S3BucketVersioningEnabled } from './S3BucketVersioningEnabled';
 export { default as S3DefaultEncryptionKMS } from './S3DefaultEncryptionKMS';
