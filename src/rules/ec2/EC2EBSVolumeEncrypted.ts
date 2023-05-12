c39d8dd701b4444c32c05462166a3f74dad17f4a
Kou Kinyo <kou.kinyo@gmail.com>
2023-05-12 22:11:32 +0900

    check LaunchTemplate

--- src/rules/ec2/EC2EBSVolumeEncrypted.ts
+++ src/rules/ec2/EC2EBSVolumeEncrypted.ts
@@ -3,8 +3,8 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 import { parse } from 'path';
-import { CfnResource } from 'aws-cdk-lib';
-import { CfnVolume } from 'aws-cdk-lib/aws-ec2';
+import { CfnResource, Stack } from 'aws-cdk-lib';
+import { CfnLaunchTemplate, CfnVolume } from 'aws-cdk-lib/aws-ec2';
 import { NagRuleCompliance, NagRules } from '../../nag-rules';
 
 /**
@@ -19,6 +19,28 @@ export default Object.defineProperty(
         return NagRuleCompliance.NON_COMPLIANT;
       }
       return NagRuleCompliance.COMPLIANT;
+    } else if (node instanceof CfnLaunchTemplate) {
+      const launchTemplateData = Stack.of(node).resolve(
+        node.launchTemplateData
+      );
+      if (launchTemplateData.blockDeviceMappings == undefined) {
+        return NagRuleCompliance.NOT_APPLICABLE;
+      } else {
+        const blockDeviceMappings = Stack.of(node).resolve(
+          launchTemplateData.blockDeviceMappings
+        );
+        for (const blockDeviceMapping of blockDeviceMappings) {
+          const resolvedDevice = Stack.of(node).resolve(blockDeviceMapping);
+          const encryption = NagRules.resolveIfPrimitive(
+            node,
+            resolvedDevice.ebs.encrypted
+          );
+          if (encryption != true) {
+            return NagRuleCompliance.NON_COMPLIANT;
+          }
+        }
+        return NagRuleCompliance.COMPLIANT;
+      }
     } else {
       return NagRuleCompliance.NOT_APPLICABLE;
     }
