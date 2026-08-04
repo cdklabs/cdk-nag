a787d99fa95a270e49487c3a51243ef9656a1b11
Momo Kornher <kornherm@amazon.co.uk>
2026-08-04 11:22:45 +0100

    more fixes

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,22 +3,28 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
-import { vscode, DevEnvironmentDockerImage } from 'projen';
+import { vscode, DevEnvironmentDockerImage, javascript } from 'projen';
 const project = new CdklabsConstructLibrary({
+  name: 'cdk-nag',
+  majorVersion: 3,
   projenrcTs: true,
   private: false,
+  packageManager: javascript.NodePackageManager.YARN_BERRY,
+
   author: 'Amazon Web Services',
   authorAddress: 'aws-cdk-dev@amazon.com',
-  defaultReleaseBranch: 'main',
-  cdkVersion: '2.257.0',
-  jsiiVersion: '5.8',
-  majorVersion: 3,
-  npmDistTag: 'latest',
-  name: 'cdk-nag',
   description:
     'Check CDK v2 applications for best practices using a combination on available rule packs.',
   repositoryUrl: 'https://github.com/cdklabs/cdk-nag.git',
+
+  cdkVersion: '2.257.0',
+  jsiiVersion: '6.0.x',
+  typescriptVersion: '6.0.x',
   devDeps: ['@aws-cdk/assert@^2.18'],
+
+  release: true,
+  defaultReleaseBranch: 'main',
+  npmDistTag: 'latest',
   publishToMaven: {
     mavenGroupId: 'io.github.cdklabs',
     javaPackage: 'io.github.cdklabs.cdknag',
@@ -32,7 +38,6 @@ const project = new CdklabsConstructLibrary({
   rosettaOptions: { strict: false },
   eslintOptions: { dirs: ['src'], prettier: true },
   buildWorkflow: true,
-  release: true,
   gitignore: ['.vscode', '**/.DS_Store'],
 });
 project.package.addField('prettier', {
@@ -46,12 +51,19 @@ project.eslint!.addRules({
     { singleQuote: true, semi: true, trailingComma: 'es5' },
   ],
 });
-project.tasks.tryFind('eslint')!.prependExec('npx prettier --write RULES.md');
+project.tasks.tryFind('eslint')!.prependExec('prettier --write RULES.md');
 const setup = project.addTask('dev-container-setup', {
   exec: 'sudo chown superchain . -R',
 });
+const setupPreCommit = project.tasks.addTask('pre-commit', {
+  condition: `node -e "process.exit(require('fs').existsSync('.git/hooks/pre-commit') ? 1 : 0)"`,
+  steps: [
+    { execArgs: ['python3', '-m', 'pip', 'install', 'pre-commit'] },
+    { execArgs: ['pre-commit', 'install'] },
+  ],
+});
 const def = project.tasks.tryFind('default')!;
-def.prependExec('python3 -m pip install pre-commit && pre-commit install');
+def.prependSpawn(setupPreCommit);
 
 const devContainer = new vscode.DevContainer(project, {
   features: [
